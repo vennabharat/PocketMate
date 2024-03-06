e2266a35918be2afabffb92329bdc225e407de52
Venna Bharat <v.bharath39@gmail.com>
2024-03-06 23:15:14 +0530

    Initial Commit

---
+++ PocketMate/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PocketMate
+//
+//  Created by bharat venna on 06/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketMate/PocketMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketMateApp.swift
+//  PocketMate
+//
+//  Created by bharat venna on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
